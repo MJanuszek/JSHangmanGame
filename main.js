619c51b1ef4677420e2b7c2a8caea8d3d4ce73c1
MJanuszek <monika.januszek.mail@gmail.com>
2022-07-29 11:52:45 +0200

    html structure, css style

---
+++ main.js
@@ -0,0 +1,4 @@
+const checkLetterBtn = document.querySelector(".check-letter");
+const inputLetter = document.querySelector(".input-letter");
+// div to display used letters::
+const usedLettersDisplay = document.querySelector(".used-letters");
