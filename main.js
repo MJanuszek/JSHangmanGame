d5bf4e0c275742cad0a2c08a1a8b9c0aff57e355
MJanuszek <monika.januszek.mail@gmail.com>
2022-08-29 14:03:53 +0200

    selectWordToPlay function in progress
    working create letter fields form selected word

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // :::: FROM HTML :::::::::::::::::::::::::::::::::::
+// lives:::
+const lives = document.querySelectorAll(".life");
 const checkLetterBtn = document.querySelector(".check-letter");
 // button to draw a word:
 const selectWord = document.querySelector(".select-word");
 const inputLetter = document.querySelector(".input-letter");
 
 // word-to-guess (word in array[]):
-const wordToGuess = document.querySelector(".word-to-guess");
+let wordToGuess = document.querySelector(".word-to-guess");
 
 // word div letters (one letter per div):
 // const wordToPlay = document.querySelectorAll(".letter")
@@ -13,32 +15,52 @@ const wordToGuess = document.querySelector(".word-to-guess");
 const usedLettersDisplay = document.querySelector(".used-letters");
 // ::::::: END FROM HTML :::::::::::::::::::::::::::::::::::
 
+
 const words = ["panter", "tiger", "elephant"];
 
-const currentWord = ["pantera"];
+const currentWord = "pantera";
 
-
-wordToGuess.textContent = currentWord[0];
 
 // selectWord()
-
 const selectWordToPlay = () => {
 
+    for (let i =0; i<currentWord.length; i++){
+        const letterField = document.createElement("div");
+        letterField.classList.add("circle");
+        wordToGuess.appendChild(letterField);
+        letterField.setAttribute("letter", currentWord[i]);
+    } 
+
+    // if (wordToGuess){
+    //     selectWord.disabled = true;
+    //     const newGameBtn = document.createElement("button");
+    //     document.body.appendChild(newGameBtn);
+    //     newGameBtn.classList.add("button");
+    //     newGameBtn.textContent = "Zacznij nową grę";
+    //     newGameBtn.addEventListener("click", startNewGame = () => {
+    //             console.log("ok");
+    //             wordToGuess.textContent = "the end";
+    //             selectWord.disabled = false;
+
+    //     });
+       
+    // };
+   
 }
-
-
-
 
 
 
 // 
 const checkLetter = () => {
     // console.log(inputLetter.value);
-    for(let i=0; i< currentWord[0].length; i++) {
-        if (inputLetter.value === i) {
-            console.log(i)
-        }
-    }
+    for(let i=0; i< currentWord.length; i++) {
+        if (inputLetter.value === currentWord[i]) {
+            console.log("ok");
+
+         } else {
+            console.log("nie ma")
+         } 
+    } 
 
     // add letter to used letters:
     usedLettersDisplay.textContent += inputLetter.value + " ";
